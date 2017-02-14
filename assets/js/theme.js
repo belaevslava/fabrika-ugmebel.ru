fd18fa2b6d2793b56cead3fb4664a1817b92f892
belaevslava <belaevslava@gmail.com>
2017-02-14 17:53:53 +0200

    +page product

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,14 +1,30 @@
-(function($) {
-	$(document).ready(function() {
-		$("[name='needDelivery']").change(function() {
-			var $this = $(this);
-			var needDelivery = $this.val() === 'true' ? true : false;
+jQuery.noConflict();
 
-			if (needDelivery) {
-				$(".delivery-block").removeClass('collapse');
-			} else {
-				$(".delivery-block").addClass('collapse');
-			}
+(function($) {
+	$(function() {
+		// Клик по кнопке 'Редактировать комплектацию'
+		$('#editComplect').bind('click', function() {
+			$('[toggle-on-edit-complect]').removeClass('hidden');
+			$('[enable-on-edit-complect]').removeAttr('disabled');
+			$(this).addClass('hidden');
 		});
+		// Клик по кнопке 'Отмена' в режиме редактирования комплектации
+		$('#cancelEditComplect').bind('click', function() {
+			$('[toggle-on-edit-complect]').addClass('hidden');
+			$('[enable-on-edit-complect]').attr('disabled', '');
+			// Удаляем пометки 'удален'-ых элементов комплекта, делаем видимыми
+			$('[removed]').removeAttr('removed').parent().removeClass('hidden');
+			$('#editComplect').removeClass('hidden');
+		});
+		// Клик по кнопке 'Удалить' элемент комплекта
+		$('.remove-complect-item').bind('click', function(e) {
+			e.preventDefault();
+			var productItemContainer = $(this).closest('.product-item'),
+				productItemCol = productItemContainer.parent();
+			// Помечаем как удаленный (не удаляя)
+			productItemContainer.attr('removed', '');
+			// Скрываем (как будто удален)
+			productItemCol.addClass('hidden');
+		})
 	});
-})(jQuery);
+})(jQuery)
